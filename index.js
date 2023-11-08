681331f09d0c3b93df6baa5c1b75f183c6e3c7b9
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-11-08 18:31:14 +0600

    post and get query in server

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvn5rcy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //mongodb
+    const coffeeCollection = client.db("onlineEduDB").collection("coffee");
+    // auth
+    const userCollection = client.db("onlineEduDB").collection("user");
+
+    const cartCollection = client.db("onlineEduDB").collection("cart");
+
+    const serviceCollection = client.db("onlineEduDB").collection("services");
+
+    const bookingCollection = client.db("onlineEduDB").collection("bookings");
+
+    //get
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // ..........................new query end............................................
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Online Assignment making server is running");
+});
+app.listen(port, () => {
+  console.log(`Online Assignment server is running port : ${port}`);
+});
